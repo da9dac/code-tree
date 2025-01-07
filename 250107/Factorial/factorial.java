49485415f864ff63d347f6677bf25703a1692275
da9dac <da9dac@gmail.com>
2025-01-07 17:01:38 +0900

    Title: Factorial, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250107/Factorial/factorial.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(solve(n));
+    }
+
+    static int solve(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        return n * solve(n - 1);
+    }
+}
