a498460d247c46503dc08e61735bde0a58a1bc29
da9dac <da9dac@gmail.com>
2025-01-08 13:22:16 +0900

    Title: 007, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250108/007/007.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        ZZS zzs = new ZZS(st.nextToken(), st.nextToken(), Integer.parseInt(st.nextToken()));
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("secret code : ").append(zzs.secret).append("\n")
+            .append("meeting point : ").append(zzs.where).append("\n")
+            .append("time : ").append(zzs.time);
+
+        System.out.println(sb);
+    }
+    
+    static class ZZS {
+        String secret;
+        String where;
+        int hour;
+
+        ZZS(String secret, String where, int hour) {
+            this.secret = secret;
+            this.where = where;
+            this.hour = hour;
+        }
+    }
+}
