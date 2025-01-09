3e6734da8537ee931bd19a751d64e5be91284fa9
da9dac <da9dac@gmail.com>
2025-01-09 13:20:11 +0900

    Title: 사는 지역, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250109/사는 지역/where-live.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+
+        People[] peoples = new People[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            peoples[i] = new People(st.nextToken(), st.nextToken(), st.nextToken());
+        }
+
+        Arrays.sort(peoples);
+
+        System.out.println(peoples[0].toString());
+    }
+
+    static class People implements Comparable<People> {
+        String name;
+        String addr;
+        String region;
+
+        People(String name, String addr, String region) {
+            this.name = name;
+            this.addr = addr;
+            this.region = region;
+        }
+
+        public int compareTo(People other) {
+            return this.name.compareTo(other.name);
+        }
+
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("name").append(" ").append(name).append("\n")
+                .append("addr").append(" ").append(addr).append("\n")
+                .append("city").append(" ").append(region);
+            
+            return sb.toString();
+        }
+    }
+}
