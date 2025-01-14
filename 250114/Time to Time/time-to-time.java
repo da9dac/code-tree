3595b64411a4ce32491bc1111416f40ec9dc14c0
da9dac <da9dac@gmail.com>
2025-01-14 15:35:40 +0900

    Title: Time to Time, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250114/Time to Time/time-to-time.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int x = a * 60 + b;
+        int y = c * 60 + d;
+
+        System.out.println(y - x);
+    }
+}
