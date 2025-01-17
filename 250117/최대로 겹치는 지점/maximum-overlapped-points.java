42f6bae0c85de5eddcdd6b33f27c553b70d2ad16
da9dac <da9dac@gmail.com>
2025-01-17 11:57:14 +0900

    Title: 최대로 겹치는 지점, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250117/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int[] line = new int[201];
+
+        int n = Integer.parseInt(br.readLine());
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x1 = Integer.parseInt(st.nextToken()) + 100;
+            int x2 = Integer.parseInt(st.nextToken()) + 100;
+
+            for (int j = x1; j <= x2; j++) {
+                line[j]++;
+            }
+        }
+
+        for (int l : line) {
+            max = Math.max(max, l);
+        }
+
+        System.out.println(max);
+    }
+}
